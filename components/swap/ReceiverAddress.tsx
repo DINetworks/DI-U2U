6227afb3766c14555d6b3afdb1ff668d597cbe52
0xawang <tik594746@gmail.com>
2025-09-27 07:07:12 +0900

    fix: eslint error

--- components/swap/ReceiverAddress.tsx
+++ components/swap/ReceiverAddress.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody } from "@heroui/react";
 import { motion } from "framer-motion";
+import { useId } from "react";
 
 interface ReceiverAddressProps {
   receiver: string;
@@ -10,6 +11,10 @@ export default function ReceiverAddress({
   receiver,
   setReceiver,
 }: ReceiverAddressProps) {
+
+	const inputId = useId()
+	const labelId = useId()
+
   return (
     <motion.div
       key="receiver-address"
@@ -24,11 +29,16 @@ export default function ReceiverAddress({
     >
       <Card className="bg-black/90 backdrop-blur-sm transition-colors mt-6">
         <CardBody className="px-6 py-4 justify-center space-y-2">
-          <label className="block text-xs font-medium text-gray-400">
+          <label 
+						className="block text-xs font-medium text-gray-400"						
+						htmlFor={inputId}
+						id={labelId}>
             Receive Wallet:
           </label>
           <input
+        		aria-labelledby={labelId}
             className="w-full py-2 bg-black/80 border border-none rounded-2xl text-gray-400 focus:text-white/80 text-xl placeholder-gray-400 focus:outline-none"
+        		id={inputId}
             placeholder="0x"
             value={receiver}
             onChange={(e) => setReceiver(e.target.value)}
